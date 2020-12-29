548b13a53b75f77cd8df9066d1a2f7d628dd7fb3
Oscar Vogel <oscarvogel@gmail.com>
2020-12-29 11:28:01 -0300

    Control de Imagen

--- pyqt5libs/Etiquetas.py
+++ pyqt5libs/Etiquetas.py
@@ -39,3 +39,47 @@ class EtiquetaRoja(Etiqueta):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setStyleSheet("color: red;")
+
+
+class Imagen(QLabel):
+
+    clicked = pyqtSignal()
+    blob_data = ''
+    path_image = ''
+    alto = 150
+    ancho = 150
+
+    def __init__(self):
+        super().__init__()
+
+    def EstablecerImagen(self, imagen=''):
+        if imagen:
+            pixmap = QPixmap(os.path.abspath(imagen)).scaled(
+                self.alto, self.ancho, Qt.KeepAspectRatio, Qt.SmoothTransformation
+            )
+            self.setPixmap(pixmap)
+
+    def mousePressEvent(self, ev: QtGui.QMouseEvent) -> None:
+        archivo = openFileNameDialog(
+            title='Importar',
+            files="Image Files (*.png)",
+        )
+        if archivo:
+            self.path_image = archivo
+            with open(archivo, 'rb') as f:
+                self.blob_data = f.read()
+            self.EstablecerImagen(imagen=archivo)
+        self.clicked.emit()
+
+    def valor(self):
+        return self.blob_data
+
+    def setText(self, blob_data: str) -> None:
+        # data1 = base64.b64decode(blob_data)
+        nombre_imagen = f'{getFileName(filename="marca")}'
+        imagen = open(nombre_imagen, 'wb')
+        imagen.write(blob_data)
+        imagen.close()
+        self.path_image = nombre_imagen
+        self.blob_data = blob_data
+        self.EstablecerImagen(imagen=nombre_imagen)
